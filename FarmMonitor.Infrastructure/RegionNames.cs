ee0ae9088d4387764cc79674e6a3e60695fe41e1
MaxTt90 <shang.yitong@163.com>
2017-05-11 19:56:41 +0800

    Add shell and Dashboard module

--- FarmMonitor.Infrastructure/RegionNames.cs
+++ FarmMonitor.Infrastructure/RegionNames.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FarmMonitor
+namespace FarmMonitor.Infrastructure
 {
     public static class RegionNames
     {
-        public const string MainRegion = "MainRegion";
+        public const string DashboardRegion = "DashboardRegion";
     }
 }
